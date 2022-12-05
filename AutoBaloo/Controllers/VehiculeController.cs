01ea64db568793aea600327295a3f94dd5771c23
sena <sanasoussi55@gmail.com>
2022-12-05 18:17:22 +0100

    Crud-module-vehicule

--- AutoBaloo/Controllers/VehiculeController.cs
+++ AutoBaloo/Controllers/VehiculeController.cs
@@ -55,20 +55,20 @@ namespace AutoBaloo.Controllers
             {
                 string uniqueFileName = null;
 
-                // If the Photo property on the incoming model object is not null, then the user
-                // has selected an image to upload.
+                // Si la propriété Photo sur l'objet de modèle entrant n'est pas nulle, alors l'utilisateur
+                // a sélectionné une image à télécharger.
                 if (model.Image != null)
                 {
-                    // The image must be uploaded to the images folder in wwwroot
-                    // To get the path of the wwwroot folder we are using the inject
-                    // HostingEnvironment service provided by ASP.NET Core
+                   // L'image doit être téléchargée dans le dossier images de wwwroot
+                     // Pour obtenir le chemin du dossier wwwroot, nous utilisons l'inject
+                     // Service HostingEnvironment fourni par ASP.NET Core
                     string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-                    // To make sure the file name is unique we are appending a new
-                    // GUID value and and an underscore to the file name
+                    // Pour nous assurer que le nom du fichier est unique, nous ajoutons un nouveau
+                    // valeur GUID et un trait de soulignement au nom du fichier
                     uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Image.FileName;
                     string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                    // Use CopyTo() method provided by IFormFile interface to
-                    // copy the file to wwwroot/images folder
+                    // Utilisez la méthode CopyTo() fournie par l'interface IFormFile pour
+                    // copie le fichier dans le dossier wwwroot/images
                     model.Image.CopyTo(new FileStream(filePath, FileMode.Create));
                 }
 
